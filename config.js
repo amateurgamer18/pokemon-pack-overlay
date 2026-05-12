8e982a4d24c8a961be348fed3255db7a73ca5366
amateurgamer18 <amateurgamer18@hotmail.co.uk>
2026-05-12 16:09:27 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -55,5 +55,5 @@ window.STREAM_CONFIG = {
   // you've uploaded to your own GitHub repo. NOTE: hosting Nintendo/Pokémon-
   // licensed artwork on public sites can attract takedown notices — that's
   // your call.
-  cardBackImageUrl: '',
+  cardBackImageUrl: 'https://www.deviantart.com/atomicmonkeytcg/art/Pokemon-Card-Backside-in-High-Resolution-633406210',
 };
